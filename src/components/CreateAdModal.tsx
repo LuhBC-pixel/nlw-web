f4726021019e5e9d5316e255f13be0fce9ac3645
Luisa Boina Coltro <bc.luisa@yahoo.com.br>
2022-09-22 16:33:50 -0300

    incomplete toast

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -1,11 +1,21 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
 import { FormCreateAd } from './Form/FormCreateAd';
 
 interface Game {
   id: string;
   title: string;
+}
+
+export interface IFormInputs {
+  game: string;
+  name: string;
+  yearsPlaying: number;
+  discord: string;
+  hourStart: string;
+  hourEnd: string;
 }
 
 export function CreateAdModal() {
@@ -21,21 +31,56 @@ export function CreateAdModal() {
     );
   }, []);
 
+  async function handleCreatAd(data: any) {
+    try {
+      const response = await axios.post(
+        `https://nlw-server-production-254a.up.railway.app/games/${data.game}/ads`,
+        {
+          name: data.name,
+          yearsPlaying: Number(data.yearsPlaying),
+          discord: data.discord,
+          weekDays: weekDays.map(Number),
+          hourStart: data.hourStart,
+          hourEnd: data.hourEnd,
+          useVoiceChannel: useVoiceChannel,
+        }
+      );
+
+      toast('Anúncio criado com sucesso!');
+    } catch (err) {
+      toast.error('Erro ao criar anúncio!');
+    }
+  }
+
   return (
-    <Dialog.Portal>
-      <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
-      <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25'>
-        <Dialog.Title className='text-3xl font-black'>
-          Publique um Anúcio
-        </Dialog.Title>
-        <FormCreateAd
-          games={games || []}
-          weekDays={weekDays}
-          useVoiceChannel={useVoiceChannel}
-          setWeekDays={setWeekDays}
-          setUseVoiceChannel={setUseVoiceChannel}
-        />
-      </Dialog.Content>
-    </Dialog.Portal>
+    <>
+      <Dialog.Portal>
+        <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
+        <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25'>
+          <Dialog.Title className='text-3xl font-black'>
+            Publique um Anúcio
+          </Dialog.Title>
+          <FormCreateAd
+            games={games || []}
+            weekDays={weekDays}
+            useVoiceChannel={useVoiceChannel}
+            setWeekDays={setWeekDays}
+            setUseVoiceChannel={setUseVoiceChannel}
+            handleCreatAd={handleCreatAd}
+          />
+        </Dialog.Content>
+      </Dialog.Portal>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 }
--- src/components/Form/FormCreateAd.tsx
+++ src/components/Form/FormCreateAd.tsx
@@ -1,26 +1,18 @@
 import axios from 'axios';
 import * as Dialog from '@radix-ui/react-dialog';
 import * as Checkbox from '@radix-ui/react-checkbox';
-
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { Check, GameController } from 'phosphor-react';
+
 import '../../styles/input.css';
 import { WeekDays } from './WeekDays';
+import { IFormInputs } from '../CreateAdModal';
 
 interface Game {
   id: string;
   title: string;
-}
-
-interface IFormInputs {
-  game: string;
-  name: string;
-  yearsPlaying: number;
-  discord: string;
-  hourStart: string;
-  hourEnd: string;
 }
 
 interface Props {
@@ -29,6 +21,7 @@ interface Props {
   useVoiceChannel: boolean;
   setWeekDays: (weekDays: string[]) => void;
   setUseVoiceChannel: (value: boolean) => void;
+  handleCreatAd: (data: IFormInputs) => void;
 }
 
 const schema = yup.object({
@@ -46,170 +39,150 @@ export function FormCreateAd({
   useVoiceChannel,
   setWeekDays,
   setUseVoiceChannel,
+  handleCreatAd,
 }: Props) {
   const {
     register,
-    watch,
     handleSubmit,
     formState: { errors },
   } = useForm<IFormInputs>({ resolver: yupResolver(schema) });
 
-  const data = watch();
-
-  async function handleCreatAd() {
-    try {
-      await axios.post(
-        `https://nlw-server-production-254a.up.railway.app/games/${data.game}/ads`,
-        {
-          name: data.name,
-          yearsPlaying: Number(data.yearsPlaying),
-          discord: data.discord,
-          weekDays: weekDays.map(Number),
-          hourStart: data.hourStart,
-          hourEnd: data.hourEnd,
-          useVoiceChannel: useVoiceChannel,
-        }
-      );
-
-      alert('Anúncio criado com sucesso!');
-    } catch (err) {
-      console.log(err);
-      alert('Erro ao criar anúncio!');
-    }
-  }
-
   return (
-    <form
-      onSubmit={handleSubmit(handleCreatAd)}
-      className='mt-8 flex flex-col gap-4'
-    >
-      <div className='flex flex-col gap-2'>
-        <label htmlFor='game' className='font-semibold'>
-          Qual o game?
-        </label>
-        <select
-          id='game'
-          className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
-          defaultValue=''
-          {...register('game')}
-        >
-          <option disabled selected value=''>
-            Selecione o game que deseja jogar
-          </option>
-          {games?.map((game) => (
-            <option key={game.id} value={game.id}>
-              {game.title}
-            </option>
-          ))}
-        </select>
-        <p className='text-red-500 text-sm'>{errors.game?.message}</p>
-      </div>
-
-      <div className='flex flex-col gap-2'>
-        <label htmlFor='name' className='font-semibold'>
-          Seu nome (ou nickname)
-        </label>
-        <input
-          type='text'
-          id='name'
-          placeholder='Como te chamam dentro do game?'
-          className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
-          {...register('name')}
-        />
-        <p className='text-red-500 text-sm'>{errors.name?.message}</p>
-      </div>
-
-      <div className='grid grid-cols-2 gap-6'>
+    <>
+      <form
+        onSubmit={handleSubmit(handleCreatAd)}
+        className='mt-8 flex flex-col gap-4'
+      >
         <div className='flex flex-col gap-2'>
-          <label htmlFor='yearsPlaying' className='font-semibold'>
-            Joga há quantos anos?
+          <label htmlFor='game' className='font-semibold'>
+            Qual o game?
           </label>
-          <input
-            type='number'
-            id='yearsPlaying'
-            placeholder='Tudo bem ser ZERO'
-            className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
-            {...register('yearsPlaying')}
-          />
-          <p className='text-red-500 text-sm'>{errors.yearsPlaying?.message}</p>
+          <select
+            id='game'
+            className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none'
+            defaultValue=''
+            {...register('game')}
+          >
+            <option disabled selected value=''>
+              Selecione o game que deseja jogar
+            </option>
+            {games?.map((game) => (
+              <option key={game.id} value={game.id}>
+                {game.title}
+              </option>
+            ))}
+          </select>
+          <p className='text-red-500 text-sm'>{errors.game?.message}</p>
         </div>
 
         <div className='flex flex-col gap-2'>
-          <label htmlFor='discord' className='font-semibold'>
-            Qual seu Discord?
+          <label htmlFor='name' className='font-semibold'>
+            Seu nome (ou nickname)
           </label>
           <input
             type='text'
-            id='discord'
-            placeholder='Usuario#0000'
+            id='name'
+            placeholder='Como te chamam dentro do game?'
             className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
-            {...register('discord')}
+            {...register('name')}
           />
-          <p className='text-red-500 text-sm'>{errors.discord?.message}</p>
+          <p className='text-red-500 text-sm'>{errors.name?.message}</p>
         </div>
-      </div>
 
-      <div className='flex gap-6'>
-        <WeekDays weekDays={weekDays} setWeekDays={setWeekDays} />
-
-        <div className='flex flex-col gap-2 flex-1'>
-          <label htmlFor='hourStart' className='font-semibold'>
-            Qual horário do dia?
-          </label>
-          <div className='grid grid-cols-2 gap-2'>
+        <div className='grid grid-cols-2 gap-6'>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor='yearsPlaying' className='font-semibold'>
+              Joga há quantos anos?
+            </label>
             <input
-              id='hourStart'
-              type='time'
-              placeholder='De'
-              defaultValue=''
+              type='number'
+              id='yearsPlaying'
+              placeholder='Tudo bem ser ZERO'
               className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
-              {...register('hourStart')}
+              {...register('yearsPlaying')}
             />
-            <input
-              id='hourEnd'
-              type='time'
-              placeholder='Até'
-              className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
-              {...register('hourEnd')}
-            />
+            <p className='text-red-500 text-sm'>
+              {errors.yearsPlaying?.message}
+            </p>
           </div>
-          <p className='text-red-500 text-sm'>
-            {errors.hourStart?.message || errors.hourEnd?.message}
-          </p>
+
+          <div className='flex flex-col gap-2'>
+            <label htmlFor='discord' className='font-semibold'>
+              Qual seu Discord?
+            </label>
+            <input
+              type='text'
+              id='discord'
+              placeholder='Usuario#0000'
+              className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+              {...register('discord')}
+            />
+            <p className='text-red-500 text-sm'>{errors.discord?.message}</p>
+          </div>
         </div>
-      </div>
 
-      <label className='mt-2 flex items-center gap-2 text-sm'>
-        <Checkbox.Root
-          onCheckedChange={(checked) => {
-            if (checked === true) {
-              setUseVoiceChannel(true);
-            } else {
-              setUseVoiceChannel(false);
-            }
-          }}
-          checked={useVoiceChannel}
-          className='w-6 h-6 p-1 rounded bg-zinc-900'
-        >
-          <Checkbox.Indicator>
-            <Check className='w-4 h-4 text-emerald-400' />
-          </Checkbox.Indicator>
-        </Checkbox.Root>
-        Constumo me conectar ao chat de voz
-      </label>
+        <div className='flex gap-6'>
+          <WeekDays weekDays={weekDays} setWeekDays={setWeekDays} />
 
-      <footer className='mt-4 flex justify-end gap-4'>
-        <Dialog.Close className='bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'>
-          Cancelar
-        </Dialog.Close>
-        <button
-          type='submit'
-          className='bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600'
-        >
-          <GameController size={24} />
-          Encontrar duo
-        </button>
-      </footer>
-    </form>
+          <div className='flex flex-col gap-2 flex-1'>
+            <label htmlFor='hourStart' className='font-semibold'>
+              Qual horário do dia?
+            </label>
+            <div className='grid grid-cols-2 gap-2'>
+              <input
+                id='hourStart'
+                type='time'
+                placeholder='De'
+                defaultValue=''
+                className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+                {...register('hourStart')}
+              />
+              <input
+                id='hourEnd'
+                type='time'
+                placeholder='Até'
+                className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+                {...register('hourEnd')}
+              />
+            </div>
+            <p className='text-red-500 text-sm'>
+              {errors.hourStart?.message || errors.hourEnd?.message}
+            </p>
+          </div>
+        </div>
+
+        <label className='mt-2 flex items-center gap-2 text-sm'>
+          <Checkbox.Root
+            onCheckedChange={(checked) => {
+              if (checked === true) {
+                setUseVoiceChannel(true);
+              } else {
+                setUseVoiceChannel(false);
+              }
+            }}
+            checked={useVoiceChannel}
+            className='w-6 h-6 p-1 rounded bg-zinc-900'
+          >
+            <Checkbox.Indicator>
+              <Check className='w-4 h-4 text-emerald-400' />
+            </Checkbox.Indicator>
+          </Checkbox.Root>
+          Constumo me conectar ao chat de voz
+        </label>
+
+        <footer className='mt-4 flex justify-end gap-4'>
+          <Dialog.Close className='bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'>
+            Cancelar
+          </Dialog.Close>
+          <button
+            type='submit'
+            className='bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600'
+          >
+            <GameController size={24} />
+            Encontrar duo
+          </button>
+        </footer>
+      </form>
+    </>
   );
 }
